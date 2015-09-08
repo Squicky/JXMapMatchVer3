01e76faadad43a08eccc68b0731f6344be11ccfe
as <as>
2015-09-08 15:55:41 +0200

    test 6

--- src/jxmapmatch/JXMapMatch.java
+++ src/jxmapmatch/JXMapMatch.java
@@ -6,7 +6,7 @@ import dialogelements.JWindowLoading;
 
 /**
  * @author Daniel Sathees Elmo
- * @author Adrian Skuballa (5)
+ * @author Adrian Skuballa (6)
  * 
  * 
  * This class launch the application by showing a splash screen
